c3c8482aa4f52075f4e320a99784dc16ad09207a
Razmalin <80904496+Razmalin@users.noreply.github.com>
2022-12-27 23:05:01 +0300

    Update oslab_3.cpp

--- lab3/OpenMP/oslab_3.cpp
+++ lab3/OpenMP/oslab_3.cpp
@@ -1,43 +1,42 @@
 #include <iostream>
-#include <windows.h>
 #include <omp.h.>
+#include <windows.h>
+#include <iomanip>
 
 using namespace std;
 
-const unsigned N = 100000000;
-const unsigned blockSize = 60;			// 03050(6)
-const unsigned threadsCount = 16;		// 1, 2, 4, 8, 12, 16
+const int threadsCount = 8;		// 1, 2, 4, 8, 12, 16
+const int blockSize = 60; 		// 03050(6)
+const int N = 100000000;
 
-int main() {
-
+double pi = 0;
+int main()
+{
 	system("chcp 437");
 	system("cls");
 	cout << "This is a console application by Artem Ivanov,\n";
 	cout << "a student of group 0305, for laboratory work\n";
 	cout << "on the study of processor management using the OpenMP.\n\n";
 
-	double pi = 0;
-	int blocksNumber = N / blockSize;
-	int start, finish;
-	omp_lock_t locker;
-
+	int CountBlock = N / blockSize;
+	int startTime, endTime;
+	omp_lock_t lock;
 	omp_set_num_threads(threadsCount);
-	omp_init_lock(&locker);
-
-	start = GetTickCount();
-
-	#pragma omp parallel for schedule(dynamic, blockSize) reduction(+:pi)
-	// calculate block using given formula
-	for (int i = 0; i < N; ++i)
-		pi += 4 / (1 + ((i + 0.5) / N) * ((i + 0.5) / N));
-
-	finish = GetTickCount();
-
+	omp_init_lock(&lock);
+	startTime = GetTickCount();
+#pragma omp parallel  
+	{
+		double keepingPi = 0;
+#pragma omp for
+		for (int block = 0; block < CountBlock; ++block)
+			for (int i = block * blockSize; i < block * blockSize + blockSize; ++i)
+				keepingPi += 4 / (1 + ((i + 0.5) / N) * ((i + 0.5) / N));
+		omp_set_lock(&lock);
+		pi += keepingPi;
+		omp_unset_lock(&lock);
+	}
+	endTime = GetTickCount();
 	pi /= N;
-
-	cout.precision(N);
-	int time = finish - start;
-	cout << "Pi - " << pi << endl << "Time - " << time << " ms." << endl;
-
-	return 0;
+	cout << "Pi - " << fixed << setprecision(20) << pi;
+	cout <<	endl << "Time -  " << endTime - startTime << " ms";
 }
